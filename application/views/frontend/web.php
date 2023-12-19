0a52bcb7cddbda50096442979bd32b4ba8774a29
sandemoid <infosandemo@gmail.com>
2023-12-19 23:35:43 +0700

    Update slide images to webp format

--- application/views/frontend/web.php
+++ application/views/frontend/web.php
@@ -88,7 +88,7 @@
             <div class="carousel-inner" role="listbox">
 
                 <!-- Slide 1 -->
-                <div class="carousel-item active" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-1.jpg)">
+                <div class="carousel-item active" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-1.webp)">
                     <div class="carousel-container">
                         <div class="container">
                             <h2 class="animate__animated animate__fadeInDown">Assalamualaikum</h2>
@@ -99,7 +99,7 @@
                 </div>
 
                 <!-- Slide 2 -->
-                <div class="carousel-item" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-2.jpg)">
+                <div class="carousel-item" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-2.webp)">
                     <div class="carousel-container">
                         <div class="container">
                             <h2 class="animate__animated animate__fadeInDown">Multiplatform</h2>
@@ -110,7 +110,7 @@
                 </div>
 
                 <!-- Slide 3 -->
-                <div class="carousel-item" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-3.jpg)">
+                <div class="carousel-item" style="background-image: url(<?= base_url('assets/frontend/') ?>img/slide/slide-3.webp)">
                     <div class="carousel-container">
                         <div class="container">
                             <h2 class="animate__animated animate__fadeInDown">Mudah Digunakan</h2>
@@ -201,8 +201,8 @@
             <div class="container">
 
                 <div class="section-title">
-                    <h2>Keuangan Masjid/Musala</h2>
-                    <p>Berikut data keuangan Masjid/Musala yang terdaftar</p>
+                    <h2>Daftar Masjid/Musala</h2>
+                    <p>Kami bangga melihat berbagai Masjid dan Musala yang telah sukses mengadopsi Aplikasi Keuangan kami dalam pengelolaan keuangan mereka. Bersama-sama, kami membentuk jejak keberhasilan, membuktikan bahwa solusi ini tidak hanya menguntungkan tetapi juga diakui oleh beragam komunitas keagamaan. Bergabunglah dengan mereka untuk menyederhanakan dan meningkatkan pengelolaan keuangan Masjid/Musala Anda.</p>
                 </div>
                 <div class="row">
 
@@ -401,24 +401,18 @@
     <!-- ======= Footer ======= -->
     <footer id="footer">
         <div class="container">
-            <h3>Green</h3>
-            <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+            <h3><?= get_setting('nama_website') ?></h3>
+            <p><?= get_setting('alamat') ?></p>
             <div class="social-links">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <a href="https://<?= get_setting('youtube') ?>" class="youtube"><i class="bx bxl-youtube"></i></a>
+                <a href="https://<?= get_setting('facebook') ?>" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://<?= get_setting('instagram') ?>" class="instagram"><i class="bx bxl-instagram"></i></a>
             </div>
             <div class="copyright">
-                &copy; Copyright <strong><span>Green</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span><?= date('Y') . ' ' . get_setting('nama_website') ?></span></strong>. All Rights Reserved
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/green-free-one-page-bootstrap-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Developer by <a target="_blank" href="https://sandemoindoteknologi.co.id/">Sandemo Indo Teknologi</a>
             </div>
         </div>
     </footer><!-- End Footer -->
